c5d04ef2be8910b3dbb6c927778f6b3a4ea9ce89
Skanarky <kutkurov@gmail.com>
2020-09-20 11:06:31 -0600

    dwld struct

--- dwnld-mngr.go
+++ dwnld-mngr.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+type Download struct {
+	OriginURI     string
+	TargetPath    string
+	TotalSections int
+}
+
 func main() {
+	startTime := time.Now()
+	// FIXME:
+	// TODO: pass URI, create package?
+	// TODO: create path dynamically 
+	download := Download{
+		OriginURI: "https://some-uri-will-gohere"
+		TargetPath    "some-path-will-go-here.jpg"
+		TotalSections 10
+	}
 	fmt.Printf("Executing download manger\n")
 }
