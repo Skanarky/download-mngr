3dc90ec8fd64fe59b50e801d545a00fdc951e009
Skanarky <kutkurov@gmail.com>
2020-09-25 12:56:13 -0600

    added methods for req and new err fn

--- dwnld-mngr.go
+++ dwnld-mngr.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 )
 
@@ -15,9 +17,42 @@ func main() {
 	startTime := time.Now()
 
 	download := Download{
-		OriginURI:     "https://some-uri-will-gohere",
-		TargetPath:    "some-path-will-go-here.jpg",
+		// OriginURI:     "https://drive.google.com/uc?id=16-ik4vC6ynIfQP3_DdziQH9KyjeIdVlM&authuser=0&export=download",
+		OriginURI:     "https://drive.google.com/file/d/16-ik4vC6ynIfQP3_DdziQH9KyjeIdVlM/view?usp=sharing",
+		TargetPath:    "the-pic.jpg",
 		TotalSections: 10,
 	}
-	fmt.Printf("Executing download manger\n")
+
+	err := download.Run()
+
+	checkError(err)
+
+	fmt.Printf("Download completed in: %v\n", time.Now().Sub(startTime).Seconds())
+}
+
+func (download Download) Run() error {
+	fmt.Println("Download started. Connecting.")
+	req, err := download.PrepareNewRequest("HEAD")
+	checkError(err)
+	res, err := http.DefaultClient.Do(req)
+	checkError(err)
+	fmt.Printf("Response Stat Code: %v\n", res.StatusCode)
+	return nil
+}
+
+func (download Download) PrepareNewRequest(method string) (*http.Request, error) {
+	req, err := http.NewRequest(
+		method,
+		download.OriginURI,
+		nil,
+	)
+	checkError(err)
+	req.Header.Set("User-Agent", "Simple Download Manager")
+	return req, nil
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalf("Error while downloading: %s\n", err)
+	}
 }
